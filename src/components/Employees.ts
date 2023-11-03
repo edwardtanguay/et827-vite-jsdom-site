dfade11f69f4f55a876dc2b16c02a592484143d5
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-03 11:59:01 +0100

    employees in

--- src/components/Employees.ts
+++ src/components/Employees.ts
@@ -3,5 +3,11 @@ import employees from '../data/employees.json';
 export const Employees = () => {
 	return `
 	<h2>There are ${employees.length} employees</h2>
+
+	<ul>
+		${employees.map(employee => {
+			return `<li>${employee.firstName} ${employee.lastName}</li>`;
+		}).join('')}
+	</ul>
 	`
 }
